99d59ed2ff228f827c51df8ffb93d9d0a6fa4e20
Eivind Arvesen <eivind.arvesen@gmail.com>
2015-01-10 22:02:46 +0100

    Initial commit after a sleepless night hacking.

---
+++ script.js
@@ -0,0 +1,19 @@
+function startTime() {
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    h = checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML = h+":"+m+":"+s;
+    var t = setTimeout(function(){startTime()},500);
+}
+
+function checkTime(i) {
+    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+// initialize search autocompletion...
+$("#search").googleSuggest({secure: true});
